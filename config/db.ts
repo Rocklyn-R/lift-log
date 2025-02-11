4a90ec94c8d9b3fe079c7cfb45cad6b0baef10ac
Rocklyn-R <antica.rusi@gmail.com>
2025-02-11 14:27:17 +0100

    Switch to redis and add production config

--- config/db.ts
+++ config/db.ts
@@ -1,15 +1,31 @@
 import { Pool, QueryResult } from 'pg';
 import dotenv from 'dotenv';
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
-dotenv.config();
+const poolConfig = process.env.NODE_ENV === 'production'
+  ? {
+    connectionString: process.env.INTERNAL_DATABASE_URL, // Use the DATABASE_URL provided by Render
+    ssl: {
+      rejectUnauthorized: false, // Required for connecting securely to Render's PostgreSQL
+    },
+  } : {
+    connectionString: process.env.EXTERNAL_DATABASE_URL, // Use the DATABASE_URL provided by Render
+    ssl: {
+      rejectUnauthorized: false, // Required for connecting securely to Render's PostgreSQL
+    },
+  }
 
+  const pool = new Pool(poolConfig);
+/*
 const pool = new Pool({
     user: process.env.DB_USER,
     host: 'localhost',
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: 5432
-})
+})*/
 
 export const query = (text: string, params?: any[]): Promise<QueryResult<any>> => {
     return pool.query(text, params);
--- server.ts
+++ server.ts
@@ -2,17 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import passport, { initialize } from 'passport';
 import session from 'express-session';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import exercisesRouter from './routes/exercises';
 import usersRouter from './routes/users';
 import { initializePassport } from './config/passport';
 import logsRouter from './routes/logs';
 import timerRouter from "./routes/timers";
 import settingsRouter from "./routes/settings";
-
+import { RedisStore } from 'connect-redis';
+import { createClient } from 'redis';
 if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
+
 
 const app = express();
 app.use(express.json());
@@ -36,7 +38,53 @@ app.use(cors(corsOptions));
 app.set('trust proxy', 1);
 app.use(express.static(__dirname));
 
+const redisUrl = process.env.NODE_ENV === 'development' ? 'rediss://red-csb5ltogph6c73aaak60:0SR0RXoGw6haTyYewERfjKB0p1LfTPPJ@virginia-redis.render.com:6379' : 'redis://red-csb5ltogph6c73aaak60:6379'; // Use external Redis URL in developmentn
+// Create a Redis client
 
+const redisClient = createClient({
+    url: redisUrl
+});
+
+// Connect to Redis
+redisClient.connect().catch(err => {
+    console.error('Could not connect to Redis:', err);
+});
+
+redisClient.on('error', (err) => {
+    console.error('Redis error:', err);
+});
+
+(async () => {
+  
+await redisClient.set('123Antica', 'Vatamo te');
+const value = await redisClient.get('123Antica');
+console.log("found value: ", value);  
+})();
+
+const cookieSecret = process.env.COOKIE_SECRET;
+
+if (!cookieSecret) {
+  throw new Error('Missing COOKIE_SECRET environment variable');
+}
+
+// Set up session middleware
+app.use(session({
+    store: new RedisStore({
+        client: redisClient,
+        prefix: 'lift-log:sess:', // Add prefix here
+      }),  
+    secret: cookieSecret,
+    proxy: true, // Required when behind a proxy like Render
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      secure: process.env.NODE_ENV === 'production', // Only send cookies over HTTPS in production
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // Allows cookies to be sent in cross-site contexts (e.g., if front end is on a different domain)
+    }
+  }));
+
+/*
  // Development session setup
  app.use(session({
      secret: process.env.COOKIE_SECRET as string,
@@ -47,7 +95,7 @@ app.use(express.static(__dirname));
          maxAge: 1000 * 60 * 60 * 24, 
          secure: false, 
      },
- }));
+ }));*/
 
  app.use(passport.initialize());
  app.use(passport.session());
